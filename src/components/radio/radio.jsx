4a23e9c8651dd5e0a585fa4423cfd67f44a5e54f
fabienbounoir <fabien.bounoir@epitech.eu>
2021-09-07 15:00:14 +0200

    change IP serv

--- src/components/radio/radio.jsx
+++ src/components/radio/radio.jsx
@@ -21,9 +21,9 @@ class Radio extends Component {
             user: this.getCookie("username") === null ? (this.setCookie("username",`Discord User ${Math.floor(Math.random() * 4000)}`), this.getCookie("username")) : (this.getCookie("username"))
         }
         this.picture = `${Math.floor(Math.random() * 5)}`
-        this.socket = io("http://localhost:3002/");
+        // this.socket = io("http://localhost:3002/");
 
-        // this.socket = io("https://backendbounsbot.herokuapp.com:3002/");
+        this.socket = io("https://backendbounsbot.herokuapp.com:3002/");
     }
 
     joinVocal = () => {
