ce8a14368c94a6e391d50747742b84efae823380
fabienbounoir <fabien.bounoir@epitech.eu>
2021-09-07 14:55:32 +0200

    ajout name personnalité + picture random + socket tchat

--- src/components/radio/radio.jsx
+++ src/components/radio/radio.jsx
@@ -1,32 +1,45 @@
 // import React from "react";
 import "./_radio.css";
 import React, { Component } from 'react'
+import io, { Socket } from 'socket.io-client';
 
 
 class Radio extends Component {
     constructor(props) {
         super(props);
-        this.radio = new Audio('https://streams.iloveradio.de/iloveradio5.mp3');
-        this.radio.volume = 0.5;
+        this.radio = new Audio(this.getCookie("radio") || 'https://streams.iloveradio.de/iloveradio5.mp3');
+        this.joinSound = new Audio("https://cdn.discordapp.com/attachments/825523344468082743/884722966523428874/discord-join-sound-effect-download.mp3");
+        this.leaveSound = new Audio("https://cdn.discordapp.com/attachments/825523344468082743/884722968155013130/discord-leave-sound-effect-hd.mp3");
+        this.radio.volume = this.getCookie("volume") ||  0.4;
+        this.joinSound.volume = 0.5;
+        this.leaveSound.volume = 0.5;
         this.inVocal = false;
         this.pause = true;
-    }
 
-    state = {
-        data: []
+        this.state = {
+            data: [],
+            user: this.getCookie("username") === null ? (this.setCookie("username",`Discord User ${Math.floor(Math.random() * 4000)}`), this.getCookie("username")) : (this.getCookie("username"))
+        }
+        this.picture = `${Math.floor(Math.random() * 5)}`
+        this.socket = io("http://localhost:3002/");
+
+        // this.socket = io("https://backendbounsbot.herokuapp.com:3002/");
     }
 
     joinVocal = () => {
         var voiceuser = document.getElementById("discord-user-voiceuser");
         voiceuser.classList.remove("hidden");
 
+        this.joinSound.play();
         var botVoiceUser = document.getElementById("discord-bot-voiceuser");
         botVoiceUser.classList.add("speaking");
 
         var voicePanel = document.getElementById("discord-voice-panel");
         voicePanel.classList.remove("hidden");
 
-        this.radio.play();
+        setTimeout(() => {
+            this.radio.play();
+        }, 1000);
         this.inVocal = true;
         this.pause = false;
     };
@@ -41,15 +54,23 @@ class Radio extends Component {
         var voicePanel = document.getElementById("discord-voice-panel");
         voicePanel.classList.add("hidden");
         this.radio.pause()
+        this.leaveSound.play();
         this.inVocal = false;
         this.pause = false;
     };
 
     _handleKeyDown = (e) => {
-        if (e.key === 'Enter' && document.getElementById('discord-inner-text-box-input').value != "") {
+        let message = document.getElementById('discord-inner-text-box-input').value;
+        if (e.key === 'Enter' && message !== "") {
             this.setState({
-                data: this.state.data.concat({name:"Discord User",picture:"user-0",text:document.getElementById('discord-inner-text-box-input').value})
+                data: this.state.data.concat({name:this.state.user,picture:`user-${this.picture}`,text:message})
             });
+
+            if(message.indexOf(`-`) !== 0)
+            {
+                this.socket.emit('tchat',{name:this.state.user,picture:`user-${this.picture}`,text:message})
+            }
+
             setTimeout(() => {
                 this.checkCommande(document.getElementById('discord-inner-text-box-input').value)
                 document.getElementById('discord-inner-text-box-input').value = "";
@@ -59,21 +80,28 @@ class Radio extends Component {
 
     checkCommande = (commande) =>
     {
-        if(commande.toLowerCase().indexOf(`-radio `) == 0)
+        if(commande.toLowerCase().indexOf(`-radio `) === 0)
         {
             this.radioCommandes(commande.split(' ')[1])
         }
-        else if(commande.toLowerCase().indexOf(`-volume `) == 0)
+        else if(commande.toLowerCase().indexOf(`-volume `) === 0)
         {
             this.volumeCommandes(commande.split(' ')[1])
         }
-        else if(commande == "-resume")
+        else if(commande === "-resume")
         {   
             this.playCommandes()
         }
-        else if(commande == "-pause")
+        else if(commande === "-pause")
         {
             this.pauseCommandes();
+        }
+        else if(commande.toLowerCase().indexOf(`/nick `) === 0)
+        {
+            this.setState({
+                user: commande.split(" ")[1]
+            });
+            this.setCookie("username",commande.split(" ")[1])
         }
     };
 
@@ -83,14 +111,14 @@ class Radio extends Component {
             {
                 this.radio.play()
                 this.setState({
-                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"▶️"})
+                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"▶️"})
                 });
                 this.pause = false
             }
             else
             {
                 this.setState({
-                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"Vous n'êtes pas dans le vocal"})
+                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"Vous n'êtes pas dans le vocal"})
                 });
             }
     }
@@ -106,7 +134,7 @@ class Radio extends Component {
                 this.radio = new Audio(this.selectRadio(number))
                 
                 this.setState({
-                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:`Radio N°${number} en cours de streaming`})
+                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:`Radio N°${number} en cours de streaming`})
                 });
 
                 if(this.inVocal)
@@ -114,20 +142,21 @@ class Radio extends Component {
                     this.radio.play()
                     this.pause = false;
                 }
+                this.setCookie("radio",this.selectRadio(number))
                 console.log(this.radio);
             }
             else
             {
                 // sendTempsMessage(5000,"La radio n'existe pas !!",undefined)
                 this.setState({
-                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"La radio n'existe pas !!"})
+                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"La radio n'existe pas !!"})
                 });
             }
         }
         else
         {
             this.setState({
-                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"Ce n'est pas un chiffre"})
+                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"Ce n'est pas un chiffre"})
             });
         }
     }
@@ -141,20 +170,21 @@ class Radio extends Component {
             {
                 this.radio.volume = volume;
                 this.setState({
-                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:`Volume réglé sur ${volume}`})
+                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:`Volume réglé sur ${volume}`})
                 });
+                this.setCookie("volume",volume)
             }
             else
             {
                 this.setState({
-                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"Volume non compris entre 0 et 1"})
+                    data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"Volume non compris entre 0 et 1"})
                 });
             }
         }
         else
         {
             this.setState({
-                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"Ce n'est pas un chiffre"})
+                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"Ce n'est pas un chiffre"})
             });
         }
     }
@@ -167,14 +197,14 @@ class Radio extends Component {
         {
             this.radio.pause()
             this.setState({
-                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"⏸"})
+                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"⏸"})
             });
             this.pause = true
         }
         else
         {
             this.setState({
-                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-1",text:"Vous n'êtes pas dans le vocal"})
+                data: this.state.data.concat({name:"Bouns'Bot",picture:"user-6",text:"Vous n'êtes pas dans le vocal"})
             });
         }
     }
@@ -186,6 +216,39 @@ class Radio extends Component {
     
     componentDidUpdate() {
         this.scrollToBottom();
+    }
+
+    componentDidMount = () => {
+
+    this.socket.on("receive", message => {
+        this.setState({
+            data: this.state.data.concat(message)
+        });
+    });
+
+        // this.socket.emit('commande');
+    }
+
+    
+    setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+
+    getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)===' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
     }
 
     selectRadio(radio)
@@ -201,13 +264,11 @@ class Radio extends Component {
             8: "https://streams.ilovemusic.de/iloveradio14.mp3",
             9: "https://streams.ilovemusic.de/iloveradio13.mp3",
             10: "https://streams.ilovemusic.de/iloveradio8.mp3",
-
             11: "https://streams.ilovemusic.de/iloveradio21.mp3",
             12: "https://streams.ilovemusic.de/iloveradio109.mp3",
             13: "https://streams.ilovemusic.de/iloveradio24.mp3",
             14: "https://streams.ilovemusic.de/iloveradio18.mp3",
             15: "https://streams.ilovemusic.de/iloveradio4.mp3",
-
             16: "https://stream-mz.planetradio.co.uk/net2national.mp3", 
             17: "http://icy-e-bab-02-gos.sharp-stream.com/absoluteradio.mp3",
             18: "http://ais.absoluteradio.co.uk/absolute70s.mp3",
@@ -215,32 +276,23 @@ class Radio extends Component {
             20: "http://ais.absoluteradio.co.uk/absolute90s.mp3",
             21: "http://ais.absoluteradio.co.uk/absolute00s.mp3",
             22: "http://icy-e-bab-04-cr.sharp-stream.com/absoluteclassicrock.mp3",
-        
             23: "http://loadbalancing.topradio.be/topradio.mp3", 
-        
             24: "http://radio886.fluidstream.eu/886_live.mp3",
             25: "http://onair.krone.at/kronehit.mp3",
-        
             26: "http://cdn.nrjaudio.fm/audio1/fr/30401/mp3_128.mp3?origine=fluxradios",
             27: "http://www.skyrock.fm/stream.php/tunein16_128mp3.mp3",
             28: "http://funradiobe.ice.infomaniak.ch/funradiobe-high.mp3",
             29: "http://cdn.nrjaudio.fm/audio1/fr/30001/mp3_128.mp3",
-        
             30: "http://icestreaming.rai.it:80/1.mp3",
             31: "http://icestreaming.rai.it:80/2.mp3",
-        
             32: "http://icecast.err.ee:80/vikerraadio.mp3",
             33: "http://icecast.err.ee:80/raadiotallinn.mp3",
-        
             34: "http://icecast8.play.cz/color128.mp3",
             35: "http://ice.abradio.cz:8000/helax128.mp3",
-        
             36: "http://icecast6.play.cz/cro2-128.mp3",
             37:"http://icecast4.play.cz/spin128.mp3",
-        
             38: "http://icecast.omroep.nl/radio1-bb-mp3",
             39: "http://21223.live.streamtheworld.com/RADIO538.mp3",
-        
             40: "http://streams2.radio90.pl:8000/radio90_128kbps_stereo.mp3",
             41: "http://stream2.nadaje.com:8076/,stream.mp3"
         }
@@ -271,7 +323,7 @@ class Radio extends Component {
                               <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M11.383 3.07904C11.009 2.92504 10.579 3.01004 10.293 3.29604L6 8.00204H3C2.45 8.00204 2 8.45304 2 9.00204V15.002C2 15.552 2.45 16.002 3 16.002H6L10.293 20.71C10.579 20.996 11.009 21.082 11.383 20.927C11.757 20.772 12 20.407 12 20.002V4.00204C12 3.59904 11.757 3.23204 11.383 3.07904ZM14 5.00195V7.00195C16.757 7.00195 19 9.24595 19 12.002C19 14.759 16.757 17.002 14 17.002V19.002C17.86 19.002 21 15.863 21 12.002C21 8.14295 17.86 5.00195 14 5.00195ZM14 9.00195C15.654 9.00195 17 10.349 17 12.002C17 13.657 15.654 15.002 14 15.002V13.002C14.551 13.002 15 12.553 15 12.002C15 11.451 14.551 11.002 14 11.002V9.00195Z">
                               </path>
                           </svg>
-                          <p style={{ marginBottom: "0px;" }}>&nbsp;&nbsp;Salon vocal</p>
+                          <p>&nbsp;&nbsp;Salon vocal</p>
                       </div>
                       <div className="demo-env member-list" role="group">
                           <div className="voiceUser clickable userSmall" id="discord-bot-voiceuser" tabIndex="-1" role="button">
@@ -283,9 +335,9 @@ class Radio extends Component {
                           </div>
                           <div className="voiceUser hidden clickable userSmall" id="discord-user-voiceuser" tabIndex="-1" role="button">
                               <div className="content">
-                                  <div className="avatarContainer avatar avatarSmall user-0-avatar" style={{ backgroundImage: "url('https://cdn.discordapp.com/embed/avatars/2.png')" }}>
+                                  <div className="avatarContainer avatar avatarSmall user-0-avatar" style={{ backgroundImage: `url('https://cdn.discordapp.com/embed/avatars/${this.picture}.png')` }}>
                                   </div>
-                                  <div className="usernameFont username">Discord User</div>
+                                  <div className="usernameFont username">{this.state.user}</div>
                               </div>
                           </div>
                       </div>
@@ -315,17 +367,17 @@ class Radio extends Component {
                   </div>
                   <div className="demo-env" id="discord-inner">
                       <div className="demo-env" id="discord-inner-messages"><div className="discord-inner-message" >
-                          <div className="avatar user-1"></div>
+                          <div className="avatar user-6"></div>
                           <div className="message-content">
                               <p className="name"><b>Bouns'Bot</b></p>
                               Bienvenue sur cette démo de la fonctionnalité radio du Bouns'Bot <br />Toutes les radios sont disponibles sur cette démo <br />Rejoignez / Quitter un channel vocal fonctionnent comme sur Discord, <br />le bot fait une pause lorsque vous quittez le vocal et reprend lorsque vous y êtes.
                           </div>
                       </div>
 
-                          <div class="discord-inner-message">
-                              <div class="avatar user-1"></div>
-                              <div class="message-content">
-                                  <p class="name"><b>Bouns'Bot</b></p>
+                          <div className="discord-inner-message">
+                              <div className="avatar user-6"></div>
+                              <div className="message-content">
+                                  <p className="name"><b>Bouns'Bot</b></p>
 
                                   <p>Cette démo dispose d'un nombre limité de commandes :<br />
                                   </p><ul>
@@ -338,7 +390,7 @@ class Radio extends Component {
                               </div>
                           </div>
                           <div className="discord-inner-message">
-                              <div className="avatar user-1"></div>
+                              <div className="avatar user-6"></div>
                               <div className="message-content">
                                   <p className="name"><b>Bouns'Bot</b></p>
                                   Je joue déjà dans le canal vocal #Salon vocal, cliquez dessus à droite pour commencer à écouter.
@@ -374,4 +426,3 @@ class Radio extends Component {
 }
 
 export default Radio;
-
