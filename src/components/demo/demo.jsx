cb9668836d22cff7865165c7f79405596538bf4a
fabienbounoir <fabien.bounoir@epitech.eu>
2021-09-11 12:41:32 +0200

    ajout bouton muet

--- src/components/demo/demo.jsx
+++ src/components/demo/demo.jsx
@@ -29,7 +29,8 @@ class Demo extends Component {
         this.state = {
             messageData: [],
             vocalData: [],
-            user: this.getCookie("username") === null ? (this.setCookie("username",`Discord User ${Math.floor(Math.random() * 4000)}`), this.getCookie("username")) : (this.getCookie("username"))
+            user: this.getCookie("username") === null ? (this.setCookie("username",`Discord User ${Math.floor(Math.random() * 4000)}`), this.getCookie("username")) : (this.getCookie("username")),
+            muet: true
         }
 
         //genere la PP du client
@@ -37,7 +38,7 @@ class Demo extends Component {
 
         //socket server link
         this.socket = io("https://socketbounsbot.herokuapp.com/");
-        // this.socket = io("http://192.168.1.164:3001/"); //en local test
+        // this.socket = io("http://127.0.0.1:3001/"); //en local test
     }
 
     //appeler lorsque l'utilisateur clique sur le bouton join (salon vocal)
@@ -45,7 +46,7 @@ class Demo extends Component {
         var voiceuser = document.getElementById("discord-user-voiceuser");
         voiceuser.classList.remove("hidden");
 
-        if(!this.inVocal) this.socket.emit('joinVocal',this.state.user,`${this.picture}`)
+        if(!this.inVocal) this.socket.emit('joinVocal', this.state.user,this.picture, this.state.muet)
         this.joinSound.play();
 
         var botVoiceUser = document.getElementById("discord-bot-voiceuser");
@@ -81,6 +82,15 @@ class Demo extends Component {
         this.inVocal = false;
         this.pause = false;
     };
+
+    toggleMuet = () => {
+        this.setState({
+            muet: !this.state.muet
+        });
+        setTimeout(() => {
+            this.socket.emit("muet",this.state.muet)
+        }, 20);
+    }
 
     //check les touches entre sur le clavier dans l'input
     _handleKeyDown = (e) => {
@@ -140,6 +150,9 @@ class Demo extends Component {
                     messageData: this.state.messageData.concat({name:"Bouns'Bot",picture:"user-6",text:"▶️"})
                 });
                 this.pause = false
+
+                var botVoiceUser = document.getElementById("discord-bot-voiceuser");
+                botVoiceUser.classList.add("speaking");
             }
             else
             {
@@ -225,6 +238,9 @@ class Demo extends Component {
                 messageData: this.state.messageData.concat({name:"Bouns'Bot",picture:"user-6",text:"⏸"})
             });
             this.pause = true
+
+            var botVoiceUser = document.getElementById("discord-bot-voiceuser");
+            botVoiceUser.classList.remove("speaking");
         }
         else
         {
@@ -248,6 +264,12 @@ class Demo extends Component {
 
     //appeler lorsque la page est chargé
     componentDidMount = () => {
+
+        //besoin du connexion securisé pour transmettre l'audio
+        if (window.location.protocol != "https:") {
+            window.location.protocol = "https:";
+        }
+
         //reception lorsque qu'un nouveau message est ecrit dans le tchat
         this.socket.on("receive", message => {
             this.setState({
@@ -277,6 +299,13 @@ class Demo extends Component {
             }
         });
 
+        //mettre à jour les utilisateur muet
+        this.socket.on("muetUpdate", data => {
+            this.setState({
+                vocalData: data
+            });
+        });
+
         //Recuperation de la voix en cour de diffusion dans le channel Vocal
         this.socket.on("getVoice", (data) => {
             if(this.inVocal)
@@ -296,7 +325,7 @@ class Demo extends Component {
     //Gestion du micro
     voix()
     {
-        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        navigator?.mediaDevices?.getUserMedia({ audio: true }).then((stream) => {
             var madiaRecorder = new MediaRecorder(stream);
             madiaRecorder.start();
         
@@ -316,8 +345,9 @@ class Demo extends Component {
                 fileReader.onloadend = () => {
             
                     var base64String = fileReader.result;
-                    if(this.inVocal) 
+                    if(this.inVocal && !this.state.muet) 
                     {
+                        console.log("transmit")
                         this.socket?.emit("voice", base64String);
                     }
 
@@ -449,6 +479,11 @@ class Demo extends Component {
                                   <div className="avatarContainer avatar avatarSmall user-0-avatar" style={{ backgroundImage: `url('https://cdn.discordapp.com/embed/avatars/${this.picture}.png')` }}>
                                   </div>
                                   <div className="usernameFont username">{this.state.user}</div>
+                                  {this.state.muet ? (
+                                        <div className="muetLogo">
+                                            <svg aria-hidden="false" width="15" height="15" viewBox="0 0 24 24"><path d="M6.7 11H5C5 12.19 5.34 13.3 5.9 14.28L7.13 13.05C6.86 12.43 6.7 11.74 6.7 11Z" fill="#b9bbbe"></path><path d="M9.01 11.085C9.015 11.1125 9.02 11.14 9.02 11.17L15 5.18V5C15 3.34 13.66 2 12 2C10.34 2 9 3.34 9 5V11C9 11.03 9.005 11.0575 9.01 11.085Z" fill="#b9bbbe"></path><path d="M11.7237 16.0927L10.9632 16.8531L10.2533 17.5688C10.4978 17.633 10.747 17.6839 11 17.72V22H13V17.72C16.28 17.23 19 14.41 19 11H17.3C17.3 14 14.76 16.1 12 16.1C11.9076 16.1 11.8155 16.0975 11.7237 16.0927Z" fill="#b9bbbe"></path><path d="M21 4.27L19.73 3L3 19.73L4.27 21L8.46 16.82L9.69 15.58L11.35 13.92L14.99 10.28L21 4.27Z" fill="red"></path></svg>
+                                        </div>
+                                  ):("")}
                               </div>
                           </div>
                           {(() => {
@@ -464,6 +499,11 @@ class Demo extends Component {
                                                 <div className="avatarContainer avatar avatarSmall user-0-avatar" style={{ backgroundImage: `url('https://cdn.discordapp.com/embed/avatars/${vocal.picture}.png')` }}>
                                                 </div>
                                                 <div className="usernameFont username">{vocal.name}</div>
+                                                {vocal.muet ? (
+                                                    <div className="muetLogo">
+                                                        <svg aria-hidden="false" width="15" height="15" viewBox="0 0 24 24"><path d="M6.7 11H5C5 12.19 5.34 13.3 5.9 14.28L7.13 13.05C6.86 12.43 6.7 11.74 6.7 11Z" fill="#b9bbbe"></path><path d="M9.01 11.085C9.015 11.1125 9.02 11.14 9.02 11.17L15 5.18V5C15 3.34 13.66 2 12 2C10.34 2 9 3.34 9 5V11C9 11.03 9.005 11.0575 9.01 11.085Z" fill="#b9bbbe"></path><path d="M11.7237 16.0927L10.9632 16.8531L10.2533 17.5688C10.4978 17.633 10.747 17.6839 11 17.72V22H13V17.72C16.28 17.23 19 14.41 19 11H17.3C17.3 14 14.76 16.1 12 16.1C11.9076 16.1 11.8155 16.0975 11.7237 16.0927Z" fill="#b9bbbe"></path><path d="M21 4.27L19.73 3L3 19.73L4.27 21L8.46 16.82L9.69 15.58L11.35 13.92L14.99 10.28L21 4.27Z" fill="red"></path></svg>
+                                                    </div>
+                                                ):("")}
                                             </div>
                                         </div>
                                     );
@@ -487,6 +527,19 @@ class Demo extends Component {
                               </div>
                           </div>
                           <div className="demo-env flex horizontal directionRow justifyStart alignStretch noWrap buttons" style={{ flex: "0 0 auto;" }}>
+                          <button onClick={this.toggleMuet} aria-label="Muet" type="button" className="demo-env button enabled lookBlank colorBrand grow" id="discord-channel-muet">
+                            {this.state.muet ? (
+                                <div className="demo-env contents">
+                                    <svg aria-hidden="false" width="20" height="20" viewBox="0 0 24 24"><path d="M6.7 11H5C5 12.19 5.34 13.3 5.9 14.28L7.13 13.05C6.86 12.43 6.7 11.74 6.7 11Z" fill="currentColor"></path><path d="M9.01 11.085C9.015 11.1125 9.02 11.14 9.02 11.17L15 5.18V5C15 3.34 13.66 2 12 2C10.34 2 9 3.34 9 5V11C9 11.03 9.005 11.0575 9.01 11.085Z" fill="currentColor"></path><path d="M11.7237 16.0927L10.9632 16.8531L10.2533 17.5688C10.4978 17.633 10.747 17.6839 11 17.72V22H13V17.72C16.28 17.23 19 14.41 19 11H17.3C17.3 14 14.76 16.1 12 16.1C11.9076 16.1 11.8155 16.0975 11.7237 16.0927Z" fill="currentColor"></path><path d="M21 4.27L19.73 3L3 19.73L4.27 21L8.46 16.82L9.69 15.58L11.35 13.92L14.99 10.28L21 4.27Z" style={{ color: "hsl(359,calc(var(--saturation-factor, 1)*82.6%),59.4%)" }} fill="currentColor"></path></svg>
+                                </div>
+                            ) : (
+                                
+                                <div className="demo-env contents">
+                                    <svg aria-hidden="false" width="20" height="20" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 13 17.72V21H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1ZM12 4C11.2 4 11 4.66667 11 5V11C11 11.3333 11.2 12 12 12C12.8 12 13 11.3333 13 11V5C13 4.66667 12.8 4 12 4Z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 13 17.72V22H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1Z" fill="currentColor"></path></svg>
+                                </div>
+                            )}
+                            </button>
+
                               <button onClick={this.leaveVocal} aria-label="Disconnect" type="button" className="demo-env button enabled lookBlank colorBrand grow" id="discord-channel-disconnect">
                                   <div className="demo-env contents"><svg aria-hidden="false" width="20" height="20" viewBox="0 0 24 24">
                                       <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M21.1169 1.11603L22.8839 2.88403L19.7679 6.00003L22.8839 9.11603L21.1169 10.884L17.9999 7.76803L14.8839 10.884L13.1169 9.11603L16.2329 6.00003L13.1169 2.88403L14.8839 1.11603L17.9999 4.23203L21.1169 1.11603ZM18 22H13C6.925 22 2 17.075 2 11V6C2 5.447 2.448 5 3 5H7C7.553 5 8 5.447 8 6V10C8 10.553 7.553 11 7 11H6C6.063 14.938 9 18 13 18V17C13 16.447 13.447 16 14 16H18C18.553 16 19 16.447 19 17V21C19 21.553 18.553 22 18 22Z">
