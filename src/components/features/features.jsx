5f2dd6bf5928f50b050979f0d1afdf3d9c4e9aeb
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2022-03-12 16:27:21 +0100

    ajout temporaire channel feature

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -13,6 +13,7 @@ import AnimationImage from "../animationimage/animationimage";
 import join from '../picture/features/JoinChannel.svg';
 import twitch1 from '../picture/features/twitch1.svg';
 import twitch2 from '../picture/features/twitch2.svg';
+import tempoVocaux from '../picture/features/tempoVocaux.svg';
 
 
 export const Features = () => {
@@ -39,7 +40,7 @@ export const Features = () => {
                                 <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
                             </svg>
                         </div> 
-                        <p>Laissez vos membres se démarquer sur votre serveur grâce au système de Leveling.<br/>Vous pourrez afficher le classement</p>
+                        <p>Laissez vos membres se démarquer sur votre serveur grâce au système de Leveling.<br/>Vous pourrez afficher le classement.</p>
                     </div>
                     <AnimationImage img={ [Level,Rank] }/>
                 </div>
@@ -52,7 +53,7 @@ export const Features = () => {
                                 <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
                             </svg>
                         </div> 
-                        <p>Un bot musique acceptant les Liens Youtube / Fichier Externe (.mp3 / .mp4 / .flac / .ogg / .aac / .webm / .wav)<br/>Plus de 40 radio disponibles !!<br/>Crée / Modifier / Jouer une playlist</p>
+                        <p>Un bot musique acceptant les Liens Youtube / Fichier Externe (.mp3 / .mp4 / .flac / .ogg / .aac / .webm / .wav)<br/>Plus de 40 radio disponibles !!<br/>Crée / Modifier / Jouer une playlist.</p>
                     </div>
                 </div>
                 <div className="feature active">
@@ -63,7 +64,7 @@ export const Features = () => {
                                 <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
                             </svg>
                         </div> 
-                        <p>Des logs de qualité avec les message supprimé / modifié, mais aussi les logs de cam et meme de partage d'ecran<br/>Auto Rename lorsqu'un pseudo contient des caractères spéciaux<br/>Nettoyer le tchat avec efficacité avec la commande "clear"</p>
+                        <p>Des logs de qualité avec les message supprimé / modifié, mais aussi les logs de cam et meme de partage d'ecran<br/>Auto Rename lorsqu'un pseudo contient des caractères spéciaux<br/>Nettoyer le tchat avec efficacité avec la commande "clear".</p>
                     </div>
                     <AnimationImage img={ [Moderation,join,Rename] }/>
                 </div>
@@ -76,8 +77,20 @@ export const Features = () => {
                                 <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
                             </svg>
                         </div> 
-                        <p>Retranscrire le tchat Twitch d'une chaine sur un channel Discord <br/>Les badges et les liens sont aussi retranscrit sur Discord<br/>Si un message est supprimer ou si un utilisateur et timeout/ban ces messages sera instant supprimer</p>
+                        <p>Retranscrire le tchat Twitch d'une chaine sur un channel Discord <br/>Les badges et les liens sont aussi retranscrit sur Discord<br/>Si un message est supprimer ou si un utilisateur et timeout/ban ces messages seront instant supprimer sur discord.</p>
                     </div>
+                </div>
+                <div className="feature active">
+                    <div className="content">
+                        <div className="title">
+                            <h3>Salon temporaires</h3> 
+                            <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
+                            </svg>
+                        </div> 
+                        <p>Créer des salons temporaires automatiques, avec un nom prédéfini et une limite de personne par défaut.</p>
+                    </div>
+                    <AnimationImage img={ [tempoVocaux] }/>
                 </div>
             </div>
         </div>
