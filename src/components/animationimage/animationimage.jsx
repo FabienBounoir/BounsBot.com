fd8cabc1dc1b5e66151df96c585c9e7d31052a2e
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2022-08-21 16:55:27 +0200

    📝 new feature design

--- src/components/animationimage/animationimage.jsx
+++ src/components/animationimage/animationimage.jsx
@@ -2,30 +2,31 @@ import React from 'react';
 import "./_animationimage.css";
 
 class AnimationImage extends React.Component {
-    state = {
-        increment: 0,
-        image: this.props.img
-    }
-  
-    tick() {
-        this.setState({
-            increment: this.state.increment + 1 >= this.state.image.length ? (0) : (this.state.increment + 1),
-        });
-    }
-    
-    componentDidMount() {
-      this.interval = setInterval(() => this.tick(), 7000);
-    }
-  
-    componentWillUnmount() {
-      clearInterval(this.interval);
-    }
-  
-    render() {
-      return (
-          <img className="image" alt="logo" src= { this.state.image[this.state.increment] }/>
-      );
-    }
+  state = {
+    increment: 0,
+    image: this.props.img,
+    time: this.props.time
+  }
+
+  tick() {
+    this.setState({
+      increment: this.state.increment + 1 >= this.state.image.length ? (0) : (this.state.increment + 1),
+    });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), this.state.time || 7000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <img className="image" alt="logo" src={this.state.image[this.state.increment]} />
+    );
+  }
 }
 
 export default AnimationImage;
--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -1,24 +1,32 @@
 import "./_features.css";
 import { Link } from "react-router-dom"
-import Reaction from '../../assets/picture/features/reaction.svg';
-import Reaction2 from '../../assets/picture/features/reaction2.svg';
-import Sheesh from '../../assets/picture/features/sheesh.svg';
-import Moderation from '../../assets/picture/features/moderation.svg';
-import Rename from '../../assets/picture/features/rename.svg';
-import Musique from '../../assets/picture/features/musique.svg';
-import Playlist from '../../assets/picture/features/playlist.svg';
-import Radio from '../../assets/picture/features/radio.svg';
-import Level from '../../assets/picture/features/level.svg';
-import Rank from '../../assets/picture/features/rank.svg';
 import AnimationImage from "../animationimage/animationimage";
-import join from '../../assets/picture/features/JoinChannel.svg';
-import twitch1 from '../../assets/picture/features/twitch1.svg';
-import twitch2 from '../../assets/picture/features/twitch2.svg';
-import tempoVocaux from '../../assets/picture/features/tempoVocaux.svg';
 
-import chifumi from '../../assets/picture/features/chifumi.svg';
-import morpion from '../../assets/picture/features/morpion.svg';
-import puissance4 from '../../assets/picture/features/puissance4.svg';
+import Reaction from '../../assets/picture/features/reaction1.png';
+import Reaction2 from '../../assets/picture/features/reaction2.png';
+import Sheesh from '../../assets/picture/features/sheesh.png';
+
+import tempoVocaux from '../../assets/picture/features/tempChannel.png';
+
+import twitch1 from '../../assets/picture/features/twitch1.png';
+import twitch2 from '../../assets/picture/features/twitch2.png';
+
+import Rename from '../../assets/picture/features/rename.png';
+import LogVocal from '../../assets/picture/features/logVocal.png';
+import LogTextuel from '../../assets/picture/features/logTextuel.png';
+import AppElement from '../../assets/picture/features/appElement.png';
+
+import Musique from '../../assets/picture/features/music.png';
+import Playlist from '../../assets/picture/features/playlist.png';
+import Radio from '../../assets/picture/features/radio.png';
+
+import Level from '../../assets/picture/features/level.png';
+import Rank from '../../assets/picture/features/rank.png';
+import Achievement from '../../assets/picture/features/achievement.png';
+
+import chifumi from '../../assets/picture/features/chifumi.png';
+import morpion from '../../assets/picture/features/morpion.png';
+import puissance4 from '../../assets/picture/features/puissance.png';
 
 
 export const Features = () => {
@@ -26,18 +34,16 @@ export const Features = () => {
         <div className="features">
             <div className="features__content">
                 <div className="feature active">
-                    <AnimationImage img={ [morpion,chifumi,puissance4] }/>
+                    <AnimationImage img={[morpion, chifumi, puissance4]} time="3000" />
                     <div className="content">
                         <div className="title">
-                            <h3>Pleins de jeux</h3> 
-                            {/* <a href="/commandes#configCommande"> */}
-                                <Link to="/commandes#configCommande">
-                                    <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
-                                    </svg>
-                                </Link>
-                            {/* </a> */}
-                        </div> 
+                            <h3>Pleins de jeux</h3>
+                            <Link to="/commandes#configCommande">
+                                <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
+                                </svg>
+                            </Link>
+                        </div>
                         <p>Tu veux affronter tes amis, tu as donc à ta disposition plusieurs jeux: le morpion, puissance4, chifumi, le motus, et bientôt d'autres 👀</p>
                     </div>
                 </div>
@@ -45,96 +51,84 @@ export const Features = () => {
                 <div className="feature active">
                     <div className="content">
                         <div className="title">
-                            <h3>Niveaux et points d'XP</h3> 
-                            {/* <a href="/commandes#levelCommande"> */}
+                            <h3>Niveaux et points d'XP</h3>
                             <Link to="/commandes#levelCommande">
-                            <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
-                                <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
-                            </svg>
-                            </Link>
-                                {/* </a> */}
-                        </div> 
-                        <p>Laissez vos membres se démarquer sur votre serveur grâce au système de Leveling.<br/>Vous pourrez afficher le classement.</p>
-                    </div>
-                    <AnimationImage img={ [Level,Rank] }/>
-                </div>
-                <div className="feature active">
-                    <AnimationImage img={ [Musique,Radio,Playlist] }/>
-                    <div className="content">
-                        <div className="title">
-                            <h3>Musique, Playlist, Radio</h3> 
-                            {/* <a href="/commandes#musiqueCommande"> */}
-                                <Link to="/commandes#musiqueCommande">
-                                    <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
+                                <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
                                     <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
-                                    </svg>
-                                        {/* </a> */}
-                                </Link>
-                        </div> 
-                        <p>Un bot musique acceptant les liens Youtube / Spotify / deezer /soundcloud et fichier externe (.mp3 / .mp4 / .flac / .ogg / .aac / .webm / .wav)<br/>Plus de 40 radios disponibles !!<br/>Créer / Modifier / Jouer une playlist.</p>
+                                </svg>
+                            </Link>
+                        </div>
+                        <p>Laissez vos membres se démarquer sur votre serveur grâce au système de Leveling.<br />Vous pourrez afficher le classement.</p>
+                    </div>
+                    <AnimationImage img={[Level, Rank, Achievement]} time="1000" />
+                </div>
+                <div className="feature active">
+                    <AnimationImage img={[Musique, Radio, Playlist]} time="1000" />
+                    <div className="content">
+                        <div className="title">
+                            <h3>Musique, Playlist, Radio</h3>
+                            <Link to="/commandes#musiqueCommande">
+                                <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
+                                </svg>
+                            </Link>
+                        </div>
+                        <p>Un bot musique acceptant les liens Youtube / Spotify / deezer /soundcloud et fichier externe (.mp3 / .mp4 / .flac / .ogg / .aac / .webm / .wav)<br />Plus de 40 radios disponibles !!<br />Créer / Modifier / Jouer une playlist.</p>
                     </div>
                 </div>
                 <div className="feature active">
                     <div className="content">
                         <div className="title">
-                            <h3>Modération</h3> 
-                            {/* <a href="/commandes#ModerationCommande"> */}
-                                <Link to="/commandes#ModerationCommande">
-                                    <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
-                                    </svg>
-                                    {/* </a> */}
-                                </Link>
-                        </div> 
-                        <p>Des logs de qualités avec les messages supprimés / modifiés, mais aussi les logs de cam et même de partage d'écran<br/>Auto Rename lorsqu'un pseudo contient des caractères spéciaux<br/>Nettoyer le tchat avec efficacité avec la commande "clear".</p>
+                            <h3>Modération</h3>
+                            <Link to="/commandes#ModerationCommande">
+                                <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
+                                </svg>
+                            </Link>
+                        </div>
+                        <p>Des logs de qualités avec les messages supprimés / modifiés, mais aussi les logs de cam et même de partage d'écran<br />Auto Rename lorsqu'un pseudo contient des caractères spéciaux<br />Nettoyer le tchat avec efficacité avec la commande "clear".</p>
                     </div>
-                    <AnimationImage img={ [Moderation,join,Rename] }/>
+                    <AnimationImage img={[LogTextuel, LogVocal, AppElement, Rename]} time="2000" />
                 </div>
                 <div className="feature active">
-                    <AnimationImage img={ [twitch1,twitch2] }/>
+                    <AnimationImage img={[twitch1, twitch2]} />
                     <div className="content">
                         <div className="title">
-                            <h3>Twitch vers Discord</h3> 
-                            {/* <a href="/commandes#configCommande"> */}
+                            <h3>Twitch vers Discord</h3>
                             <Link to="/commandes#configCommande">
                                 <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
-                                <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
                                 </svg>
-                                {/* </a> */}
                             </Link>
-                        </div> 
-                        <p>Retranscrire le tchat Twitch d'une chaine sur un channel Discord <br/>Les badges et les liens sont aussi retranscrits sur Discord<br/>Si un message est supprimé ou si un utilisateur est timeout/ban ces messages seront instantanement supprimés sur Discord.</p>
+                        </div>
+                        <p>Retranscrire le tchat Twitch d'une chaine sur un channel Discord <br />Les badges et les liens sont aussi retranscrits sur Discord<br />Si un message est supprimé ou si un utilisateur est timeout/ban ces messages seront instantanement supprimés sur Discord.</p>
                     </div>
                 </div>
                 <div className="feature active">
                     <div className="content">
                         <div className="title">
-                            <h3>Salons temporaires</h3> 
-                            {/* <a href="/commandes#tempChannelCommande"> */}
-                                <Link to="/commandes#tempChannelCommande">
-                                    <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
-                                    </svg>
-                                </Link>
-                            {/* </a> */}
-                        </div> 
+                            <h3>Salons temporaires</h3>
+                            <Link to="/commandes#tempChannelCommande">
+                                <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
+                                </svg>
+                            </Link>
+                        </div>
                         <p>Créer des salons temporaires automatiques, avec un nom prédéfini et une limite de personne par défaut.</p>
                     </div>
-                    <AnimationImage img={ [tempoVocaux] }/>
+                    <AnimationImage img={[tempoVocaux]} />
                 </div>
                 <div className="feature active">
-                    <AnimationImage img={ [Reaction,Reaction2,Sheesh] }/>
+                    <AnimationImage img={[Reaction, Reaction2, Sheesh]} />
                     <div className="content">
                         <div className="title">
-                            <h3>Action Réaction</h3> 
-                            {/* <a href="/commandes"> */}
-                                <Link to="/commandes">
-                                    <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
-                                    </svg>
-                                </Link>
-                            {/* </a> */}
-                        </div> 
+                            <h3>Action Réaction</h3>
+                            <Link to="/commandes">
+                                <svg width="36" height="29" viewBox="0 0 36 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="show-commands">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.02431 0.524414H0V7.52441H7.02431V0.524414ZM35.9998 0.524414H10.5366V7.52441H35.9998V0.524414ZM0 11.0244H7.02431V18.0244H0V11.0244ZM35.9998 11.0244H10.5366V18.0244H35.9998V11.0244ZM0 21.5244H7.02431V28.5244H0V21.5244ZM35.9998 21.5244H10.5366V28.5244H35.9998V21.5244Z" fill="var(--color-principal)"></path>
+                                </svg>
+                            </Link>
+                        </div>
                         <p>Un message de Bonjour / Bonne Nuit et BounsBot réagit !!!</p>
                     </div>
                 </div>
