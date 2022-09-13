60dac6ce2f9e80cfbe5535c1ee9ab908ba1340db
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2022-09-13 15:07:17 +0200

    🚀 update design + add calcule niveau

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -76,55 +76,37 @@ export const HomePage = () => {
                 <a className="home__top__bot__invite" href="https://discord.com/api/oauth2/authorize?client_id=1012688780471308339&permissions=1945627743&scope=bot%20applications.commands" ><svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.9485 8.6582C8.45993 8.6582 8.07422 9.07487 8.07422 9.5832C8.07422 10.0915 8.4685 10.5082 8.9485 10.5082C9.43708 10.5082 9.82279 10.0915 9.82279 9.5832C9.83136 9.07487 9.43708 8.6582 8.9485 8.6582ZM12.0771 8.6582C11.5885 8.6582 11.2028 9.07487 11.2028 9.5832C11.2028 10.0915 11.5971 10.5082 12.0771 10.5082C12.5656 10.5082 12.9514 10.0915 12.9514 9.5832C12.9514 9.07487 12.5656 8.6582 12.0771 8.6582Z" fill="white"></path> <path d="M17.4515 0H3.66863C2.51149 0 1.56006 0.916667 1.56006 2.05833V15.5833C1.56006 16.725 2.50292 17.6417 3.66863 17.6417H15.3343L14.7858 15.7917L16.1058 16.9833L17.3486 18.1L19.5601 20V2.05833C19.5601 0.916667 18.6172 0 17.4515 0ZM13.4829 13.0583C13.4829 13.0583 13.1143 12.625 12.8058 12.25C14.1515 11.8833 14.6658 11.0583 14.6658 11.0583C14.2458 11.325 13.8429 11.5167 13.4829 11.65C12.9686 11.8583 12.4715 12 11.9915 12.0833C11.0058 12.2667 10.0972 12.2167 9.32577 12.075C8.74291 11.9667 8.2372 11.8083 7.8172 11.6417C7.5772 11.55 7.32006 11.4417 7.06291 11.3C7.02863 11.2833 7.00291 11.2667 6.96863 11.25C6.95149 11.2417 6.93434 11.2333 6.92577 11.2167C6.7372 11.1167 6.63434 11.05 6.63434 11.05C6.63434 11.05 7.13149 11.85 8.43434 12.2333C8.12577 12.6167 7.74863 13.0667 7.74863 13.0667C5.4772 13 4.61149 11.55 4.61149 11.55C4.61149 8.33333 6.09434 5.71667 6.09434 5.71667C7.5772 4.63333 8.98291 4.66667 8.98291 4.66667L9.08577 4.78333C7.23434 5.3 6.3772 6.09167 6.3772 6.09167C6.3772 6.09167 6.60006 5.975 6.98577 5.8C8.08291 5.33333 8.9572 5.2 9.3172 5.16667C9.3772 5.15833 9.42863 5.15 9.48863 5.15C10.1143 5.06667 10.8258 5.05 11.5629 5.13333C12.5401 5.24167 13.5858 5.525 14.6572 6.09167C14.6572 6.09167 13.8429 5.34167 12.0943 4.825L12.2401 4.66667C12.2401 4.66667 13.6458 4.63333 15.1286 5.71667C15.1286 5.71667 16.6115 8.325 16.6115 11.55C16.6286 11.5417 15.7543 12.9917 13.4829 13.0583Z" fill="white"></path></svg> <span>Ajouter à Discord</span></a>
                 <div className="wave">
 
-                    <svg width="2064" height="219" viewBox="0 0 2064 219" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0_190_7)">
-                            <path d="M0 87.5003V219L2064 219V95.5002C2021 108 1879.8 125.7 1659 96.5003C1442.2 21.3003 1233 19.5003 1155.5 28.0003C991.667 75.1669 624.4 152.1 466 82.5003C307.6 12.9003 89.3333 56.8336 0 87.5003Z" fill="#40454A" />
-                            <g filter="url(#filter0_d_190_7)">
-                                <path style={{ fill: "var(--color-principal)" }} d="M439 52.9998C289.4 -10.2002 84 37.3331 0 68.9998V111C99.2 80.1998 212.667 69.6664 257 67.9998C330.6 67.1998 412 90.9409 445 99.9998C470.5 107 545.7 132.3 678.5 127.5C811.3 122.7 991.5 85.8331 1065 67.9998C1131.17 53.9998 1293.2 30.3998 1412 47.9998C1560.5 69.9998 1573.5 98.9998 1710.5 132.5C1820.1 159.3 1991.83 129.333 2064 111V68.9998C1979.6 92.5998 1902.83 102.5 1875 104.5C1775 104.5 1733.7 111.7 1544.5 40.4997C1355.3 -30.7003 1098.33 10.8331 993.5 40.4997C871 70.9997 588.6 116.2 439 52.9998Z" />
-                            </g>
-                        </g>
-                        <defs>
-                            <filter id="filter0_d_190_7" x="-10" y="-8.81348" width="2084" height="161.978" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                                <feOffset />
-                                <feGaussianBlur stdDeviation="5" />
-                                <feComposite in2="hardAlpha" operator="out" />
-                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_190_7" />
-                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_190_7" result="shape" />
-                            </filter>
-                            <clipPath id="clip0_190_7">
-                                <rect width="2064" height="219" fill="white" />
-                            </clipPath>
-                        </defs>
-                    </svg>
+                    {(() => {
+                        let svg = [];
 
-                    <svg width="2064" height="219" viewBox="0 0 2064 219" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0_190_7)">
-                            <path d="M0 87.5003V219L2064 219V95.5002C2021 108 1879.8 125.7 1659 96.5003C1442.2 21.3003 1233 19.5003 1155.5 28.0003C991.667 75.1669 624.4 152.1 466 82.5003C307.6 12.9003 89.3333 56.8336 0 87.5003Z" fill="#40454A" />
-                            <g filter="url(#filter0_d_190_7)">
-                                <path style={{ fill: "var(--color-principal)" }} d="M439 52.9998C289.4 -10.2002 84 37.3331 0 68.9998V111C99.2 80.1998 212.667 69.6664 257 67.9998C330.6 67.1998 412 90.9409 445 99.9998C470.5 107 545.7 132.3 678.5 127.5C811.3 122.7 991.5 85.8331 1065 67.9998C1131.17 53.9998 1293.2 30.3998 1412 47.9998C1560.5 69.9998 1573.5 98.9998 1710.5 132.5C1820.1 159.3 1991.83 129.333 2064 111V68.9998C1979.6 92.5998 1902.83 102.5 1875 104.5C1775 104.5 1733.7 111.7 1544.5 40.4997C1355.3 -30.7003 1098.33 10.8331 993.5 40.4997C871 70.9997 588.6 116.2 439 52.9998Z" />
-                            </g>
-                        </g>
-                        <defs>
-                            <filter id="filter0_d_190_7" x="-10" y="-8.81348" width="2084" height="161.978" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                                <feOffset />
-                                <feGaussianBlur stdDeviation="5" />
-                                <feComposite in2="hardAlpha" operator="out" />
-                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_190_7" />
-                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_190_7" result="shape" />
-                            </filter>
-                            <clipPath id="clip0_190_7">
-                                <rect width="2064" height="219" fill="white" />
-                            </clipPath>
-                        </defs>
-                    </svg>
+                        for (let i = 0; i < 2; i++) {
+                            svg.push(<svg width="2064" height="219" viewBox="0 0 2064 219" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_190_7)">
+                                    <path d="M0 87.5003V219L2064 219V95.5002C2021 108 1879.8 125.7 1659 96.5003C1442.2 21.3003 1233 19.5003 1155.5 28.0003C991.667 75.1669 624.4 152.1 466 82.5003C307.6 12.9003 89.3333 56.8336 0 87.5003Z" fill="#40454A" />
+                                    <g filter="url(#filter0_d_190_7)">
+                                        <path style={{ fill: "var(--color-principal)" }} d="M439 52.9998C289.4 -10.2002 84 37.3331 0 68.9998V111C99.2 80.1998 212.667 69.6664 257 67.9998C330.6 67.1998 412 90.9409 445 99.9998C470.5 107 545.7 132.3 678.5 127.5C811.3 122.7 991.5 85.8331 1065 67.9998C1131.17 53.9998 1293.2 30.3998 1412 47.9998C1560.5 69.9998 1573.5 98.9998 1710.5 132.5C1820.1 159.3 1991.83 129.333 2064 111V68.9998C1979.6 92.5998 1902.83 102.5 1875 104.5C1775 104.5 1733.7 111.7 1544.5 40.4997C1355.3 -30.7003 1098.33 10.8331 993.5 40.4997C871 70.9997 588.6 116.2 439 52.9998Z" />
+                                    </g>
+                                </g>
+                                <defs>
+                                    <filter id="filter0_d_190_7" x="-10" y="-8.81348" width="2084" height="161.978" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                        <feOffset />
+                                        <feGaussianBlur stdDeviation="5" />
+                                        <feComposite in2="hardAlpha" operator="out" />
+                                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_190_7" />
+                                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_190_7" result="shape" />
+                                    </filter>
+                                    <clipPath id="clip0_190_7">
+                                        <rect width="2064" height="219" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>)
+                        }
 
-
+                        return svg;
+                    })()}
                 </div>
             </div>
         </div>
