e7cf9d2be4536ad78f4b6a3a5d4d5fb234b3ed22
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2022-10-07 15:33:22 +0200

    👀 remove icon on command for performance

--- src/pages/commandes/commandes.jsx
+++ src/pages/commandes/commandes.jsx
@@ -1,8 +1,6 @@
 import "./_commandes.css";
 import { commands } from "./../../components/command.json"
 import { Component } from "react";
-import Avatar from "../../components/avatar/avatar";
-
 
 class Commandes extends Component {
     state = {
@@ -91,7 +89,6 @@ class Commandes extends Component {
                                 commandListing.push(
                                     <div key={i} className={"command-card__container " + (this.state.commands[i].open ? 'active' : '')} onClick={() => { this.openCommand(i) }}>
                                         <div className="command-card__header"><div>
-                                            <Avatar classElement="logo" width="26" height="24" />
                                             <h5 className="command-card__header__title">{`${(this.state.commands[i].name[0] + "").toUpperCase()}${(this.state.commands[i].name + "").substring(1)}`} <span>- {`${(this.state.commands[i].description[0] + "").toUpperCase()}${(this.state.commands[i].description + "").substring(1)}`}</span></h5>
                                         </div>
                                             <svg className="arrow" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15px" height="15px" viewBox="0 0 30.021 30.021"><path d="M29.069,22.276c-0.791,0.932-1.917,1.409-3.052,1.409c-0.913,0-1.834-0.312-2.587-0.949l-8.42-7.152l-8.42,7.151 c-1.683,1.43-4.208,1.225-5.639-0.459c-1.43-1.686-1.224-4.208,0.46-5.64l11.01-9.351c1.493-1.269,3.686-1.269,5.178,0 l11.011,9.351C30.294,18.068,30.499,20.591,29.069,22.276z" fill="var(--color-principal)" /></svg>
