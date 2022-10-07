c9699cf0d434c9320f132772defc6e7c373a0b17
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2022-10-07 14:31:33 +0200

    ⁉️ back to react-script

--- src/service-worker.js
+++ src/service-worker.js
@@ -43,7 +43,7 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(import.meta.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
 registerRoute(
