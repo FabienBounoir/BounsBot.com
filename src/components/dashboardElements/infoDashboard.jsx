e2a6df94c68c9d49577aece5b3c8e1588766db71
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2023-05-04 00:47:37 +0200

    📝 Update info Dashboard + Remove empty scroll bar

--- src/components/dashboardElements/infoDashboard.jsx
+++ src/components/dashboardElements/infoDashboard.jsx
@@ -3,12 +3,23 @@ export const InfoDashboard = (props) => {
         <div className="infoDashboard" >
             <h1>Dashboard</h1>
             <div className="infoDashboard__content">
-                <p>Le <b>Dashboard</b> de Bouns'bot a été entierement reinventer pour vous offrir une meilleure expérience.</p>
-                <p>Vous pouvez désormais gérer votre serveur depuis le dashboard avec plus de facilité.</p>
-                <p>Il a été concu pour être le plus simple possible et vous permettre de gérer votre serveur sans aucune difficulté.</p>
-                <br />
-                <p>Il est neanmoins encore en beta, et nous vous invitons à nous faire part de vos retours sur le serveur <a href="https://discord.gg/KxedRVTutX">Discord</a> de Bouns'bot.</p>
+                <p>Salut toi, utilisateur de Bouns'bot !</p>
+
+                <p>Je suis ravi de vous annoncer que notre Dashboard a été entièrement réinventé pour vous offrir une expérience de gestion de serveur encore plus facile et plus amusante !</p>
+
+                <p>Plus besoin de vous tracasser pour la gestion de votre serveur, notre Dashboard est conçu pour être aussi simple que possible, afin que vous puissiez gérer votre serveur sans aucun souci.</p>
+
+                <p>Nous avons travaillé dur pour que cette version soit la meilleure, et nous sommes heureux de vous la présenter, sans bêta !</p>
+
+                <p>Alors, qu'attendez-vous ? Profitez de la meilleure expérience de gestion de serveur jamais vue !</p>
+
+                <p>Et si vous avez des commentaires ou des suggestions, n'hésitez pas à nous en faire part sur notre serveur Discord. Nous sommes toujours à l'écoute de nos utilisateurs et sommes impatients de vous entendre !</p>
+
+                <p>À bientôt sur Bouns'bot !</p>
+
             </div>
+
+            <img src="https://cdn.discordapp.com/attachments/1014101467126304798/1103447646355603550/dashboard.svg" alt="Dashboard" />
         </div>
     )
 }
