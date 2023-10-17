e5ecc38ee3c942bc0f7478f0a8fc5b43692bd93c
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2023-10-17 20:42:20 +0200

    🐛 Fix error snow

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -88,7 +88,7 @@ export const App = () => {
         return Math.floor(Math.random() * (b - a + 1)) + a
       }
 
-      var embCSS = '.embedim-snow{position: absolute;width: 10px;height: 10px;background: white;border-radius: 50%;margin-top:-10px}';
+      var embCSS = '.embedim-snow{position: absolute;width: 10px;height: 10px;background: white;border-radius: 50%;margin-top:-10px;}';
       var embHTML = '';
 
       let nbSnow = 100;
@@ -97,12 +97,14 @@ export const App = () => {
 
       for (let i = 1; i < nbSnow; i++) {
 
-        embHTML += '<i className="embedim-snow"></i>'; var rndX = (embRand(0, 1000000) * 0.0001), rndO = embRand(-100000, 100000) * 0.0001, rndT = (embRand(3, 8) * 10).toFixed(2), rndS = (embRand(0, 10000) * 0.0001).toFixed(2);
+        embHTML += '<i class="embedim-snow"></i>';
 
-        embCSS += '.embedim-snow:nth-child(' + i + '){opacity:' + (embRand(1, 10000) * 0.0001).toFixed(2) + ';transform:translate(' + rndX.toFixed(2) + 'vw,-10px) scale(' + rndS + ');animation:fall-' + i + ' ' + embRand(10, 30) + 's -' + embRand(0, 30) + 's linear infinite}@keyframes fall-' + i + '{' + rndT + '%{transform:translate(' + (rndX + rndO).toFixed(2) + 'vw,' + rndT + 'vh) scale(' + rndS + ')}to{transform:translate(' + (rndX + (rndO / 2)).toFixed(2) + 'vw, 105vh) scale(' + rndS + ')}}'
+        var rndX = (embRand(0, 1000000) * 0.0001), rndO = embRand(-100000, 100000) * 0.0001, rndT = (embRand(3, 8) * 10).toFixed(2), rndS = (embRand(0, 10000) * 0.0001).toFixed(2);
+
+        embCSS += ' .embedim-snow:nth-child(' + i + '){opacity:' + (embRand(1, 10000) * 0.0001).toFixed(2) + ';transform:translate(' + rndX.toFixed(2) + 'vw,-10px) scale(' + rndS + ');animation:fall-' + i + ' ' + embRand(10, 30) + 's -' + embRand(0, 30) + 's linear infinite;}\n@keyframes fall-' + i + '{' + rndT + '%{transform:translate(' + (rndX + rndO).toFixed(2) + 'vw,' + rndT + 'vh) scale(' + rndS + ')}to{transform:translate(' + (rndX + (rndO / 2)).toFixed(2) + 'vw, 105vh) scale(' + rndS + ')}} '
       }
       embedimSnow = document.createElement('div'); embedimSnow.id = 'embedim--snow';
-      embedimSnow.innerHTML = '<style>#embedim--snow{position:fixed;pointer-events: none;left:0;top:0;bottom:0;width:100vw;height:100vh;overflow:hidden;z-index:9999999;pointer-events:none}' + embCSS + '</style>' + embHTML;
+      embedimSnow.innerHTML = '<style>#embedim--snow{position:fixed;left:0;top:0;bottom:0;width:100vw;height:100vh;overflow:hidden;z-index:9999999;pointer-events:none;}' + embCSS + '</style>' + embHTML;
       document.body.appendChild(embedimSnow)
     }
   }
