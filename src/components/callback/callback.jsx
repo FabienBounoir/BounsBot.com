25a01de56cd41a0ecd419fc5d5772036b48a9d72
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2023-11-19 12:40:54 +0100

    style: 💄 remove debug command

--- src/components/callback/callback.jsx
+++ src/components/callback/callback.jsx
@@ -1,6 +1,5 @@
 import "./_callback.css";
 import { Component } from 'react'
-import Fetch from "../../utils/fetch.js";
 import Avatar from "../avatar/avatar";
 import { login } from "../../utils/API/authAPI";
 
@@ -26,42 +25,6 @@ class Callback extends Component {
         })
     }
 
-    sendCode() {
-        const code = new URLSearchParams(window.location.search).get('code')
-
-        login(code).then(data => {
-            console.log(data)
-            this.setState({
-                type: "sendCode",
-                data: data
-            })
-        })
-    }
-
-    logout() {
-        fetch("http://localhost:3500/auth/logout").then(res => res.json()).then(data => {
-            console.log(data)
-            this.setState({
-                type: "logout",
-                data: data
-            })
-        })
-
-    }
-
-    getGuilds() {
-        fetch("http://localhost:3500/auth/guilds", {
-            headers: {
-                Authorization: "Bearer " + window.localStorage.getItem("token")
-            }
-        }).then(res => res.json()).then(data => {
-            console.log(data)
-            this.setState({
-                type: "getGuilds",
-                data: data
-            })
-        })
-    }
 
     render() {
         return (
@@ -69,16 +32,6 @@ class Callback extends Component {
                 <Avatar classElement="width-logo-svg" />
                 <p><strong>Authentification en cours</strong><br />Veuillez patienter...</p>
                 <span>Se connecter avec Discord</span>
-
-                <button onClick={() => { this.sendCode() }} >sendCode</button>
-                <button onClick={() => { this.logout() }} >logout</button>
-                <button onClick={() => { this.getGuilds() }} >Guilds</button>
-
-                <pre>
-                    {JSON.stringify(this.state, null, 2)}
-                </pre>
-
-
             </div>
         )
     }
